60f6b0a5853c17ecc27d3c32412bf9f7f2a24a42
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-09-28 16:20:18 +0200

    test

---
+++ hack/build/ci/push-helm-chart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+readonly PATH_TO_HELM_CHART="${1}"
+readonly REGISTRY_URL="${2}"
+
+# Run the command and capture stdout and stderr separately
+output=$(helm push "${PATH_TO_HELM_CHART}" oci://registry.hub.docker.com/gabrielkrenn390 2>&1)
+exit_status=$?
+
+if [ $exit_status -eq 0 ]; then
+  # Command succeeded, extract the digest
+  digest=$(echo "$output" | awk '/Digest:/ {print $2}')
+  # write to digest to github actions output 
+  echo "::set-output name=digest::$digest"
+else
+  # Command failed, print the error message and exit with the error code
+  echo "Command failed with exit status $exit_status. Error: $output"
+  exit $exit_status
+fi
