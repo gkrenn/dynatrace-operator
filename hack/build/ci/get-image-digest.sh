bb0f7d6eed1e94f67ed2f219c092b3248c95f2f7
Gabriel Krenn <gabriel.krenn@gmail.com>
2023-11-30 12:46:20 +0100

    test

--- hack/build/ci/get-image-digest.sh
+++ hack/build/ci/get-image-digest.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+readonly online="${1}"
+
+if [ -z "$2" ]
+then
+  echo "Usage: $0 <fetch_from_registry>"
+  exit 1
+fi
+
 digest=$(skopeo inspect docker-daemon:"${IMAGE}" --format "{{.Digest}}")
 echo "${DIGEST_KEY}=${digest}">> "$GITHUB_OUTPUT"
-
