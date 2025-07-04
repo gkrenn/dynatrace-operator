26a3c80d49feecfc4ebfca1f8d55c39dbc002dac
Gabriel Krenn <gabriel.krenn@gmail.com>
2025-07-04 18:09:51 +0200

    test

--- .github/actions/buildkit-cache-dance/tests/import.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it } from "vitest"
-
-describe("import", () => {
-    it("buildkit-cache-dance should be able to run", async () => {
-        process.chdir("./tests/fixtures/import")
-        await import("../dist/index.js")
-    })
-})
-
