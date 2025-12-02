e9a224a6aff6532d76b3639e7977f8e6b126b994
Gabriel Krenn <gabriel.krenn@gmail.com>
2025-12-02 22:08:17 +0100

    bug

--- .github/scripts/run-e2e-tests.sh
+++ .github/scripts/run-e2e-tests.sh
@@ -7,7 +7,6 @@ echo "Preparing test tenant secrets..."
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Executing prepare-e2e-secrets.sh from '$__dir'"
 bash ${__dir}/prepare-e2e-secrets.sh
-./prepare-e2e-secrets.sh
 
 echo hello from run-e2e-tests.sh
 ls -lah
